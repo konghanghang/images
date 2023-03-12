e3e92bb02748dad892cc8ad5f44e72d087e52432
konghanghang <yslao@outlook.com>
2023-03-12 22:30:51 +0800

    reality配置

--- xray/start.sh
+++ xray/start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-
-read -p "请输入SSL证书的域名: " XRAY_SSL_HOST
-export XRAY_SSL_HOST=$XRAY_SSL_HOST
+xray_reality=1
+xray_vision=2
+xray_vmess=3
+media_enable=1
+read -p "请选择模式 1:reality  2:vision 3:vmess: " XRAY_MODEL
+export XRAY_MODEL=${XRAY_MODEL:-1}
+if [ $XRAY_MODEL -eq $xray_vision ] 
+then
+    read -p "请输入SSL证书的域名: " XRAY_SSL_HOST
+    export XRAY_SSL_HOST=$XRAY_SSL_HOST
+fi
 read -p "是否开启流媒体转发[0：不开启，1：开启，默认0]: " XRAY_MEDIA
 XRAY_MEDIA=${XRAY_MEDIA:-0}
-read -p "是否开启vision[0：不开启，1：开启，默认0]: " XRAY_VISION
-XRAY_VISION=${XRAY_VISION:-0}
 
-media_enable=1
 if [ $XRAY_MEDIA -eq $media_enable ]
 then
     echo "流媒体转发：开启"
@@ -18,16 +23,20 @@ else
 fi
 
 # 生成config.json文件
-vision_enable=1
-if [ $XRAY_VISION -eq $vision_enable ]
+if [ $XRAY_MODEL -eq $xray_reality ]
 then
-    echo "vision：开启"
+    echo "reality开启"
+    envsubst < conf/config.json.template.reality > conf/config.json
+elif [ $XRAY_MODEL -eq $xray_vision ] 
+then
+    echo "vision开启"
     envsubst < conf/config.json.template.vision > conf/config.json
 else
-    echo "vision：关闭"
+    echo "vmess开启"
     envsubst < conf/config.json.template > conf/config.json
 fi
 
 # 取消export设置
+unset -v XRAY_MODEL
 unset -v XRAY_SSL_HOST
 unset -v XRAY_MEDIA_TAG
